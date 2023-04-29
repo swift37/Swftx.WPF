2a71ac38d9dcce5be7a209e928d59a093f86151c
Swift <rafchik37@gmail.com>
2023-04-29 18:38:53 +0200

    v1.0.2

--- Swftx.Wpf/Commands/LambdaCommand.cs
+++ Swftx.Wpf/Commands/LambdaCommand.cs
@@ -1,105 +1,104 @@
 ﻿using System.ComponentModel;
 
-namespace Swftx.Wpf.Commands
+namespace Swftx.Wpf.Commands;
+
+public class LambdaCommand : Command
 {
-    public class LambdaCommand : Command
+    private readonly Action<object?>? _Execute;
+    private readonly Func<object?, bool>? _CanExecute;
+
+    public LambdaCommand(Action Execute, Func<bool>? CanExecute = null)
+        : this(param => Execute(), CanExecute is null ? null : param => CanExecute())
     {
-        private readonly Action<object?>? _Execute;
-        private readonly Func<object?, bool>? _CanExecute;
-
-        public LambdaCommand(Action Execute, Func<bool>? CanExecute = null)
-            : this(param => Execute(), CanExecute is null ? null : param => CanExecute())
-        {
-        }
-
-        public LambdaCommand(Action<object?>? Execute, Func<object?, bool>? CanExecute = null)
-        {
-            _Execute = Execute;
-            _CanExecute = CanExecute;
-        }
-
-        protected override bool CanExecute(object? param) => _CanExecute?.Invoke(param) ?? true;
-
-        protected override void Execute(object? param) 
-        {
-            if (_Execute is null) throw new InvalidOperationException("Command execution method not defined");
-            if (!CanExecute(param)) return;
-
-            try
-            {
-                _Execute(param);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
     }
 
-    public class LambdaCommand<T> : Command
+    public LambdaCommand(Action<object?>? Execute, Func<object?, bool>? CanExecute = null)
     {
-        private readonly Action<T?>? _Execute;
-        private readonly Func<T?, bool>? _CanExecute;
+        _Execute = Execute;
+        _CanExecute = CanExecute;
+    }
 
-        public LambdaCommand(Action<T?> Execute, Func<bool>? CanExecute)
-            : this(Execute, CanExecute is null ? null : new Func<T?, bool>(_ => CanExecute()))
+    protected override bool CanExecute(object? param) => _CanExecute?.Invoke(param) ?? true;
+
+    protected override void Execute(object? param)
+    {
+        if (_Execute is null) throw new InvalidOperationException("Command execution method not defined");
+        if (!CanExecute(param)) return;
+
+        try
         {
+            _Execute(param);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+}
+
+public class LambdaCommand<T> : Command
+{
+    private readonly Action<T?>? _Execute;
+    private readonly Func<T?, bool>? _CanExecute;
+
+    public LambdaCommand(Action<T?> Execute, Func<bool>? CanExecute)
+        : this(Execute, CanExecute is null ? null : new Func<T?, bool>(_ => CanExecute()))
+    {
+    }
+
+    public LambdaCommand(Action<T?> Execute, Func<T?, bool>? CanExecute = null)
+    {
+        _Execute = Execute;
+        _CanExecute = CanExecute;
+    }
+
+    public static T? ConvertParameter(object? param)
+    {
+        switch (param)
+        {
+            case null: return default!;
+            case T result: return result;
         }
 
-        public LambdaCommand(Action<T?> Execute, Func<T?, bool>? CanExecute = null)
-        {
-            _Execute = Execute;
-            _CanExecute = CanExecute;
-        }
+        var commandType = typeof(T?);
+        var paramType = param.GetType();
 
-        public static T? ConvertParameter(object? param)
-        {
-            switch (param)
-            {
-                case null: return default!;
-                case T result: return result;
-            }
+        if (commandType.IsAssignableFrom(paramType))
+            return (T?)param;
 
-            var commandType = typeof(T?);
-            var paramType = param.GetType();
+        var commandTypeConv = TypeDescriptor.GetConverter(commandType);
+        if (commandTypeConv.CanConvertFrom(paramType))
+            return ((T?)commandTypeConv.ConvertFrom(param))!;
 
-            if (commandType.IsAssignableFrom(paramType))
-                return (T?)param;
+        var paramConv = TypeDescriptor.GetConverter(paramType);
+        if (paramConv.CanConvertTo(commandType))
+            return (T?)paramConv.ConvertFrom(param)!;
 
-            var commandTypeConv = TypeDescriptor.GetConverter(commandType);
-            if (commandTypeConv.CanConvertFrom(paramType))
-                return ((T?)commandTypeConv.ConvertFrom(param))!;
+        return default!;
+    }
 
-            var paramConv = TypeDescriptor.GetConverter(paramType);
-            if (paramConv.CanConvertTo(commandType))
-                return (T?)paramConv.ConvertFrom(param)!;
+    protected override bool CanExecute(object? param)
+    {
+        var canExecute = _CanExecute;
+        if (canExecute is null) return true;
+        if (param is null || param is T parameter && canExecute(parameter)) return true;
+        param = ConvertParameter(param);
+        return canExecute((T)param!);
+    }
 
-            return default!;
-        }
-
-        protected override bool CanExecute(object? param)
-        {
-            var canExecute = _CanExecute;
-            if (canExecute is null) return true;
-            if (param is null || param is T parameter && canExecute(parameter)) return true;
+    protected override void Execute(object? param)
+    {
+        if (_Execute is null) throw new InvalidOperationException("Command execution method not defined");
+        if (param != null && !(param is T))
             param = ConvertParameter(param);
-            return canExecute((T)param!);
-        }
 
-        protected override void Execute(object? param)
+        try
         {
-            if (_Execute is null) throw new InvalidOperationException("Command execution method not defined");
-            if (param != null && !(param is T)) 
-                param = ConvertParameter(param);
-
-            try
-            {
-                _Execute(ConvertParameter(param));
-            }
-            catch (Exception)
-            {
-                throw;  
-            }
+            _Execute(ConvertParameter(param));
+        }
+        catch (Exception)
+        {
+            throw;
         }
     }
 }
--- Swftx.Wpf/Extensions/ObservableCollectionExtensions.cs
+++ Swftx.Wpf/Extensions/ObservableCollectionExtensions.cs
@@ -1,17 +1,16 @@
-﻿namespace System.Collections.ObjectModel
-{
-    public static class ObservableCollectionExtensions
-    {
-        public static void Add<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
-        {
-            foreach (var item in items)
-                collection.Add(item);
-        }
+﻿namespace System.Collections.ObjectModel;
 
-        public static void ClearAdd<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
-        {
-            collection.Clear();
-            collection.Add(items);
-        }
+public static class ObservableCollectionExtensions
+{
+    public static void Add<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+    {
+        foreach (var item in items)
+            collection.Add(item);
+    }
+
+    public static void ClearAdd<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+    {
+        collection.Clear();
+        collection.Add(items);
     }
 }
