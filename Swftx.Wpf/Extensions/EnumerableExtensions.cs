e3cd7879daebd0f388ccc0d1333df419c8c1433c
Swift <rafchik37@gmail.com>
2023-04-29 16:45:25 +0200

    EnumerableExtensions, ObservableCollectionExtensions

---
+++ Swftx.Wpf/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace System.Collections.Generic
+{
+    public static class EnumerableExtensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> items)
+        {
+            return new ObservableCollection<T>(items);
+        }
+
+        public static ObservableCollection<T> ToObservableCollection<T>(this List<T> items)
+        {
+            return new ObservableCollection<T>(items);
+        }
+    }
+}
---
+++ Swftx.Wpf/Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Collections.ObjectModel
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void Add<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                collection.Add(item);
+        }
+
+        public static void ClearAdd<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+        {
+            collection.Clear();
+            collection.Add(items);
+        }
+    }
+}
